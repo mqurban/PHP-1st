1599c9c8efa82221618c54b5af1edcf77026afc1
Your Name <you@example.com>
2023-12-25 20:03:14 +0500

    PHP greater number find

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+ $first_numer = 10;
+ $second_numer = 20;
+ $third_numer = 30;
+
+ if ($first_numer >= $second_numer && $first_numer >= $third_numer) {
+echo "First number is greater";
+ }
+  else if ($second_numer >= $third_numer && $second_numer >= $third_numer) {
+    echo "Second is greater";
+
+ }
+ else 
+  echo "Third number is the greater"
+?>
